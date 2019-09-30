7d4a1ad6f5016e0e68e494b938b607e2948e6490
jakapun <jakapun.c@gmail.com>
2019-09-30 14:07:51 +0700

    name & detail

--- lib/screens/form_page.dart
+++ lib/screens/form_page.dart
@@ -11,14 +11,45 @@ class FormPage extends StatefulWidget {
 class _FormPageState extends State<FormPage> {
   // explicit
 
-  File file;  
+  File file;
 
   // method
+
+  Widget nameText() {
+    return Container(
+      margin: EdgeInsets.only(left: 50.0, right: 50.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Name:',
+          helperText: 'Text Your Display Name',
+          hintText: 'English Only',
+          icon: Icon(Icons.face),
+        ),
+      ),
+    );
+  }
+
+  Widget detailText() {
+    return Container(
+      margin: EdgeInsets.only(left: 50.0, right: 50.0),
+      child: TextFormField(
+        keyboardType: TextInputType.multiline,
+        maxLines: 4,
+        decoration: InputDecoration(
+          labelText: 'Detail:',
+          helperText: 'Text Your Detail',
+          hintText: 'English Only',
+          icon: Icon(Icons.details),
+        ),
+      ),
+    );
+  }
+
   Widget showImage() {
     return Container(
       margin: EdgeInsets.only(top: 50.0),
       height: 200.0,
-      child: file == null ? Image.asset('images/pic.png') : Image.file(file) ,
+      child: file == null ? Image.asset('images/pic.png') : Image.file(file),
     );
   }
 
@@ -35,7 +66,11 @@ class _FormPageState extends State<FormPage> {
 
   Widget cameraButton() {
     return IconButton(
-      icon: Icon(Icons.add_a_photo, size: 48.0, color: Colors.pink.shade300,),
+      icon: Icon(
+        Icons.add_a_photo,
+        size: 48.0,
+        color: Colors.pink.shade300,
+      ),
       onPressed: () {
         cameraThread();
       },
@@ -43,31 +78,31 @@ class _FormPageState extends State<FormPage> {
   }
 
   // thread
-  Future<void> cameraThread()async{
-
+  Future<void> cameraThread() async {
     var imageObject = await ImagePicker.pickImage(source: ImageSource.camera);
     setState(() {
-       file = imageObject; 
+      file = imageObject;
     });
-
   }
 
   Widget galleryButton() {
     return IconButton(
-      icon: Icon(Icons.add_photo_alternate, size: 48.0, color: Colors.green,),
+      icon: Icon(
+        Icons.add_photo_alternate,
+        size: 48.0,
+        color: Colors.green,
+      ),
       onPressed: () {
         galleryThread();
       },
     ); // Iconbutton == Click
   }
 
-  Future<void> galleryThread()async{
-
+  Future<void> galleryThread() async {
     var imageObject = await ImagePicker.pickImage(source: ImageSource.gallery);
     setState(() {
-       file = imageObject; 
+      file = imageObject;
     });
-
   }
 
   @override
@@ -76,6 +111,8 @@ class _FormPageState extends State<FormPage> {
       children: <Widget>[
         showImage(),
         showButton(),
+        nameText(),
+        detailText(),
       ],
     );
   }
