c81fadbaf6018e3422e96f8676219362bf10129a
jakapun <jakapun.c@gmail.com>
2019-10-01 11:55:48 +0700

    Get data from Firestore

--- lib/screens/listview_page.dart
+++ lib/screens/listview_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ListViewPage extends StatefulWidget {
@@ -6,6 +7,35 @@ class ListViewPage extends StatefulWidget {
 }
 
 class _ListViewPageState extends State<ListViewPage> {
+  // explicit
+
+  // method
+  @override
+  void initState() {
+    super.initState();
+    readDataFireStore();
+  }
+
+  // create instance
+
+  Future<void> readDataFireStore() async {
+    Firestore firestore = Firestore.instance;
+
+    // create collect referrence -> because not collection name
+    CollectionReference collectionReference = firestore.collection('Unseen');
+    // snapshot == dbs -> collections -> document per document
+    await collectionReference.snapshots().listen((response) {
+
+      // snapshots == array because put s
+      List<DocumentSnapshot> snapshots = response.documents;
+      for (var snapshot in snapshots) {
+        String name = snapshot.data['Name'];
+        print('name = $name');
+      }
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Text('List View Page');
